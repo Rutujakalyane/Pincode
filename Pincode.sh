7c8bf61da178cd8b4beb0609fc45d8fa42db4e39
Rutujakalyane <60542373+Rutujakalyane@users.noreply.github.com>
2020-04-12 19:39:06 +0530

    ADDED : Pincode with group of three and space

--- Pincode.sh
+++ Pincode.sh
@@ -1,18 +1,15 @@
 shopt -s extglob
 #VARIABLES
 pinCode=0
+
 function validatePinCode() {
-	local pin=$1
-	local regexPattern="^[1-9]{1}[0-9]{5}$"
-	if [[ $pin =~ $regexPattern ]]
+	read -p "Enter PIN Code: " pinCode
+	local regexPattern="^[1-9]{1}[0-9]{2}\s?[0-9]{3}$"
+	if [[ $pinCode =~ $regexPattern ]]
 	then
 		echo "Valid"
 	else
 		echo "Invalid"
 	fi
 }
-function enterPinCode() {
-	read -p "Enter PIN Code: " pinCode
-	validatePinCode $pinCode
-}
-enterPinCode
+validatePinCode
